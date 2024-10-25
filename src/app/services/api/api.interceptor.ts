b28e848e2db7fc404e8f2fc07a93eedcdcef0d2b
Danilo Pessoa <danilosilvapessoa@gmail.com>
2024-10-25 19:08:06 -0300

    configurando lint, testes e commit-lint

--- src/app/services/api/api.interceptor.ts
+++ src/app/services/api/api.interceptor.ts
@@ -1,5 +1,32 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpEventType,
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest
+} from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
-export const apiInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+export const apiInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> => {
+  const newRequest = req.clone({
+    setHeaders: {
+      Authorization: ''
+    }
+  });
+
+  return next(newRequest).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.info(req.url, 'returned a response with status', event.status);
+        }
+      },
+      error: (error) => {
+        console.info('Request failed', error);
+      }
+    })
+  );
 };
--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private http: HttpClient = inject(HttpClient);
+  private readonly API: string | null = environment.API_HOST;
 
-  constructor() { }
+  public get<T>(endpoint: string): Observable<T> {
+    return this.http.get(`${this.API}/${endpoint}`, {
+      headers: this.getHeaders(),
+      observe: 'response'
+    }) as Observable<T>;
+  }
+
+  public post<T, IData>(endpoint: string, data: IData): Observable<T> {
+    return this.http.post(`${this.API}/${endpoint}`, data, {
+      headers: this.getHeaders(),
+      observe: 'response'
+    }) as Observable<T>;
+  }
+
+  public put<T, IData>(endpoint: string, data: IData): Observable<T> {
+    return this.http.put(`${this.API}/${endpoint}`, data, {
+      headers: this.getHeaders(),
+      observe: 'response'
+    }) as Observable<T>;
+  }
+
+  public delete<T>(endpoint: string): Observable<T> {
+    return this.http.delete(`${this.API}/${endpoint}`, {
+      headers: this.getHeaders(),
+      observe: 'response'
+    }) as Observable<T>;
+  }
+
+  private getHeaders(): HttpHeaders {
+    // const headers = new HttpHeaders();
+    // const token = this.tokenService.getToken();
+    // if (token) {
+    //   headers.set('Authorization', `Bearer ${token}`);
+    // }
+
+    return new HttpHeaders();
+  }
 }
